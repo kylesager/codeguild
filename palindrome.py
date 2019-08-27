674bd6f75f1ef4c93bf2bc1b4b8d15c0031b88db
kylesager <54287428+kylesager@users.noreply.github.com>
2019-08-27 11:08:14 -0700

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -7,7 +7,7 @@ def reverse(word_reversed):
 ## after looking back i see how confusing my naming was 
 #word is the users input
 #word reversed is the user's input spelled backwards
-#reverse is the function that takes the length of the words (user's word, and reversed version) and checks to see if they are equal
+#reverse is the function that checks to see if the reverse spelling and user's input match.
 
 
 word = input('Enter a word: ')
